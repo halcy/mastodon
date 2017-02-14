12e7f78ce509ae38e3bc8d2d1d3b2609745b27d9
Lorenz Diener <halcyon@icosahedron.website>
2017-02-14 09:58:57 +0000

    Helper scripts

---
+++ upgrade.sh
@@ -0,0 +1,7 @@
+sudo echo "applying asset changes..."
+sudo chown -R mastodon:mastodon *
+sudo su - mastodon -c 'cd ~mastodon/live && yarn'
+sudo su - mastodon -c 'cd ~mastodon/live && RAILS_ENV=production bundle exec rails db:migrate'
+sudo su - mastodon -c 'cd ~mastodon/live && RAILS_ENV=production bundle exec rails assets:precompile'
+sudo systemctl restart mastodon-web.service
+sudo systemctl restart mastodon-sidekiq.service
