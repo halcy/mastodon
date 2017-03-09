a7fa6c750e0bbf2cd8bfc34c5bb403673653ae3a
Your Name <lorenzd@gmail.com>
2017-03-09 11:01:56 +0000

    Improved upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,7 @@
 sudo echo "applying asset changes..."
 sudo chown -R mastodon:mastodon *
 sudo su - mastodon -c 'cd ~mastodon/live && yarn'
+sudo su - mastodon -c 'cd ~mastodon/live && RAILS_ENV=production bundle install'
 sudo su - mastodon -c 'cd ~mastodon/live && RAILS_ENV=production bundle exec rails db:migrate'
 sudo su - mastodon -c 'cd ~mastodon/live && RAILS_ENV=production bundle exec rails assets:precompile'
 sudo systemctl restart mastodon-web.service
