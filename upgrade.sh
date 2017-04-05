4632c2c2c05cde33a620cba178cbb4daf4e0da46
Lorenz Diener <halcyon@icosahedron.website>
2017-04-05 12:40:51 +0000

    Fixed "registration closed" message in about page

--- upgrade.sh
+++ upgrade.sh
@@ -6,3 +6,4 @@ sudo su - mastodon -c 'cd ~mastodon/live && RAILS_ENV=production bundle exec rai
 sudo su - mastodon -c 'cd ~mastodon/live && RAILS_ENV=production bundle exec rails assets:precompile'
 sudo systemctl restart mastodon-web.service
 sudo systemctl restart mastodon-sidekiq.service
+sudo systemctl restart mastodon-streaming.service
