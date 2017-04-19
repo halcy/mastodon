df805f37d52fa00c356cab56ea870e883c27901d
Your Name <lorenzd@gmail.com>
2017-04-19 08:19:56 +0000

    Fixes some

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,7 @@
 sudo echo "applying asset changes..."
 sudo chown -R mastodon:mastodon *
+sudo su - mastodon -c 'cd ~mastodon/live && gem install bundler'
+sudo su - mastodon -c 'cd ~mastodon/live && bundle install --deployment --without development test'
 sudo su - mastodon -c 'cd ~mastodon/live && yarn'
 sudo su - mastodon -c 'cd ~mastodon/live && RAILS_ENV=production bundle install'
 sudo su - mastodon -c 'cd ~mastodon/live && RAILS_ENV=production bundle exec rails db:migrate'
