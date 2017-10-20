8c8f4b1eb385575f5fdd64ca818f782bab9ec78c
Lorenz Diener <halcyon@icosahedron.website>
2017-10-20 13:32:47 +0000

    Upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
-#sudo echo "applying asset changes..."
-#sudo chown -R mastodon:mastodon *
-#sudo su - mastodon -c 'cd ~mastodon/live && gem install bundler'
-#sudo su - mastodon -c 'cd ~mastodon/live && bundle install --deployment --without development test'
-#sudo su - mastodon -c 'cd ~mastodon/live && yarn'
-#sudo su - mastodon -c 'cd ~mastodon/live && RAILS_ENV=production bundle install'
-#sudo su - mastodon -c 'cd ~mastodon/live && RAILS_ENV=production bundle exec rails db:migrate'
+!/bin/bash
+sudo echo "applying asset changes..."
+sudo chown -R mastodon:mastodon *
+sudo su - mastodon -c 'cd ~mastodon/live && gem install bundler'
+sudo su - mastodon -c 'cd ~mastodon/live && bundle install --deployment --without development test'
+sudo su - mastodon -c 'cd ~mastodon/live && yarn'
+sudo su - mastodon -c 'cd ~mastodon/live && RAILS_ENV=production bundle install'
+sudo su - mastodon -c 'cd ~mastodon/live && RAILS_ENV=production bundle exec rails db:migrate'
 sudo su - mastodon -c 'cd ~mastodon/live && RAILS_ENV=production bundle exec rails assets:precompile'
 sudo systemctl restart mastodon-web.service
 sudo systemctl restart mastodon-sidekiq.service
